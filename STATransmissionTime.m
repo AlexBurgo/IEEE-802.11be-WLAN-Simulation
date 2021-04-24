11e6846cdd0b36b6adfcbe516769fa50c8fb9382
Alex Burgo <79794175+AlexBurgo@users.noreply.github.com>
2021-04-25 01:20:49 +0200

    Update STATransmissionTime.m

--- STATransmissionTime.m
+++ STATransmissionTime.m
@@ -1,8 +1,1 @@
 
-
-
-function [DL_TxToSTA, UL_TxToSTA, Nframes] = STATransmissionTime(nSTAs, NSS, P_rx, Length, distance, optimization)
-
-[DL_TxToSTA, UL_TxToSTA, Nframes] = wifiTransmissionTimeBasic(nSTAs, NSS, P_rx, Length, distance, optimization);
-
-end
