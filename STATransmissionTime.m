0bb5058c9a2730ebbd696e5603620b93c652991f
Alex Burgo <79794175+AlexBurgo@users.noreply.github.com>
2021-04-25 13:06:48 +0200

    Delete STATransmissionTime.m

--- STATransmissionTime.m
+++
@@ -1,8 +0,0 @@
-
-
-
-function [DL_TxToSTA, UL_TxToSTA] = STATransmissionTime(nSTAs, NSS, P_rx, Length, distance, opti)
-
-[DL_TxToSTA, UL_TxToSTA] = wifiTransmissionTimeBasic(nSTAs, NSS, P_rx, Length, distance, opti);
-
-end
